ea89a6eec8b3057db043fa86e38acc0dec468f15
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-07-21 18:29:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,26 +1,15 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+var prefix = ("+")
 
 bot.on('ready', function () {
+  bot.user.SetActivity("+help")
   console.log("Je suis connecté !")
 })
 
-bot.login('NDQ0NTk1ODQ1MDQ4OTU4OTc2.DdeXew.-uCxyHNrUtsgV1lf-oz3Qqpf5vw')
-
-bot.on('message', message => {
-  if (message.content === '+ping') {
-    message.reply('pong !')
+bot.on('message', msg => {
+  if (msgmsg.content === prefix + 'ping') {
+    msg.reply('pong !')
   }
 })
-
-bot.on('message', message => {
-  if (message.content === '+avatar') {
-    message.reply(message.author.avatarURL);
-  }
-})
-
-bot.on('guildMemberAdd', member => {
-  member.createDM().then(channel => {
-    return channel.send('Bienvenue sur mon serveur ' + member.displayName)
-  }).catch(console.error)
-})
+bot.login('process.env.TOKEN')
